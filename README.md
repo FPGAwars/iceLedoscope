e70e5f644b20cd4eddb771fd965eae3d870e3613
obijuan <obijuan.cube@gmail.com>
2021-10-30 11:18:48 +0200

    Readme updated

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# iceLedoscope
-Icestudio collection for capturing digital signals and show them on the LEDs
+[![](https://github.com/FPGAwars/iceLedoscope/raw/main/wiki/Logo/iceLedoscope-collection-github.png)](https://github.com/FPGAwars/iceLedoscope/wiki)
+
+
+# ice-ledoscopek Collection
+
+[![Icestudio][icestudio-image]][icestudio-url]
+![Version][version-image]
+
+
+Logic analyzer on the LEDs
+## Documentation
+Find all the information on the [WIKI page](https://github.com/FPGAwars/iceLedoscope/wiki)  
+
+
+## License
+
+Licensed under [LGPL-3.0](https://opensource.org/licenses/LGPL-3.0).
+
+## Install
+
+For installing and using this colection in Icestudio follow these steps:
+
+* Download the collection: [stable](https://github.com/FPGAwars/iceLedoscope/archive/refs/tags/v0.0.1.zip) or [development](https://github.com/FPGAwars/iceLedoscope/archive/refs/heads/main.zip)
+* Install the collection: *Tools > Collections > Add*
+* Select the collection: *Select > Collection*
+
+
+## Blocks
+* **Ledoscope**
+  * **08-bits**
+    * ledoscope-sys-zero-8
+
+
+## Authors
+* [Juan Gonzalez-Gomez (Obijuan)](https://github.com/Obijuan)
+
+
+
+-------
+![](https://github.com/FPGAwars/icestudio-wiki/raw/main/Logos/fgpawars-banner.svg)
+
+
+<!-- Badges -->
+[icestudio-image]: https://img.shields.io/badge/collection-icestudio-blue.svg
+[icestudio-url]: https://github.com/FPGAwars/icestudio
+[version-image]: https://img.shields.io/badge/version-v0.0.1-orange.svg
---
+++ locale/translation.js
@@ -0,0 +1,53 @@
+// Translation document for the collection
+// =======================================
+// This file contains the texts
+// annotated for translation
+//
+// Instructions:
+// 1. Open the PO file with Poedit
+// 2. Press "Update" to update from sources
+
+gettext('Ledoscope');
+gettext('08-bits');
+gettext('ledoscope-sys-zero-8');
+gettext('Ledoscope. Capture the input signal during the first 8 cycles after circuit initialization');
+gettext('Constant bit 0');
+gettext('3-bits Syscounter');
+gettext('DFF-03: Three D flip-flops in paralell');
+gettext('D Flip-flop (verilog implementation)');
+gettext('Bus3-Split-all: Split the 3-bits bus into three wires');
+gettext('Bus3-Join-all: Joint three wires into a 3-bits Bus');
+gettext('Inc1-3bit: Increment a 3-bits number by one');
+gettext('AdderK-3bit: Adder of 3-bit operand and 3-bit constant');
+gettext('Generic: 3-bits generic constant (0-7)');
+gettext('Adder-3bits: Adder of two operands of 3 bits');
+gettext('Adder-1bit: Adder of two operands of 1 bit');
+gettext('AdderC-1bit: Adder of two operands of 1 bit plus the carry in');
+gettext('XOR gate: two bits input xor gate');
+gettext('Two bits input And gate');
+gettext('OR2: Two bits input OR gate');
+gettext('SReg-right-x8: 8 bits Shift register (to the right)');
+gettext('Bus8-Join-half: Join the two same halves into an 8-bits Bus');
+gettext('SReg-right-x4: 4 bits Shift register (to the right)');
+gettext('Reg: 1-Bit register');
+gettext('2-to-1 Multplexer (1-bit channels). Fippled version');
+gettext('2-to-1 Multplexer (1-bit channels)');
+gettext('NOT gate (Verilog implementation)');
+gettext('Bus4-Join-all: Join all the wires into a 4-bits Bus');
+gettext('RS-FF-set. RS Flip-flop with priority set');
+gettext('Constant bit 1');
+gettext('8-bits Shift register');
+gettext('The input channel is captured  \non the register. One bit per  \nsystem clock');
+gettext('RS FlipFlop initialized to 1');
+gettext('while 1, the shift register  \nis capturing');
+gettext('3-bits counter');
+gettext('After 8 cycles the Flip-Flop is  \nreset and it stops capturing  \nbits');
+gettext('Parameter: Initial value');
+gettext('System clock');
+gettext('Input data');
+gettext('Output');
+gettext('# D Flip-Flop  \n\nIt stores the input data that arrives at cycle n  \nIts output is shown in the cycle n+1');
+gettext('Mux 2-1');
+gettext('D Flip-flip\n(System)');
+gettext('Input');
+gettext('Priority for the set');
